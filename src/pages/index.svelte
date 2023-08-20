73e30bb09e740113cfd5f7c5fbce4e9baf3e625c
Dian Chun Choy <dianchun.choy@ninjavan.co>
2023-08-20 17:34:27 +0800

    routify works

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+  import { goto } from '@roxi/routify'
+</script>
+
 <div>
   <p class="text-xl text-red-600">Hello</p>
+  <br />
+  <button on:click={() => $goto('/mess')}>Go to /mess</button>
 </div>
--- src/pages/mess/index.svelte
+++ src/pages/mess/index.svelte
@@ -1,1 +1,7 @@
+<script lang="ts">
+  import { goto } from '@roxi/routify'
+</script>
+
 <div class="text-blue-600">Mess mess mess</div>
+<br />
+<button on:click={() => $goto('/')}>Go to main page</button>
