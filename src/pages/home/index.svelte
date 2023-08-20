31e96b14b219e663e4180b27faaf2ac2ea6fb0f8
Dian Chun Choy <dianchun.choy@ninjavan.co>
2023-08-20 17:45:36 +0800

    added fetch pokemon and svelte query

---
+++ src/pages/home/index.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import { goto } from '@roxi/routify'
+  </script>
+  
+  <div class="text-black">HOME</div>
+  <br />
+  <button on:click={() => $goto('/')}>Go to main page</button>
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
   import { goto } from '@roxi/routify'
+  import { useQuery } from '@sveltestack/svelte-query'
+
+  const res = useQuery('pokemon', () => fetch('https://pokeapi.co/api/v2/pokemon?limit=151').then(res => res.json()))
 </script>
 
 <div>
   <p class="text-xl text-red-600">Hello</p>
   <br />
   <button on:click={() => $goto('/mess')}>Go to /mess</button>
+  <br />
+  <br />
+  <div>
+    Pokemon: {JSON.stringify($res?.data?.results)}
+  </div>
 </div>
