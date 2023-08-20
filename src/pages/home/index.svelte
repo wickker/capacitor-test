271f454244cbe50dfdacafffcf25eaf09033de55
Dian Chun Choy <dianchun.choy@ninjavan.co>
2023-08-20 18:03:57 +0800

    added svelte query and fb login

--- src/pages/home/index.svelte
+++ src/pages/home/index.svelte
@@ -1,7 +1,26 @@
 <script lang="ts">
-    import { goto } from '@roxi/routify'
-  </script>
-  
-  <div class="text-black">HOME</div>
-  <br />
-  <button on:click={() => $goto('/')}>Go to main page</button>
+  import { goto } from '@roxi/routify'
+  import Cookies from 'js-cookie'
+  import { useQuery } from '@sveltestack/svelte-query'
+  import axios from 'axios'
+
+  const otterApi = axios.create({
+    baseURL: 'http://localhost:9000',
+    withCredentials: true,
+  })
+
+  const getInvoices = (): Promise<any> =>
+    otterApi.get(`1.0/sellers/invoices`).then((res) => res.data)
+
+  const res = useQuery('invoices', getInvoices)
+</script>
+
+<div class="text-black">HOME</div>
+<p>Greeting cookie: {Cookies.get('greeting')}</p>
+<br />
+<button on:click={() => $goto('/')}>Go to main page</button>
+<br />
+<br />
+<div>
+  Invoices: {JSON.stringify($res?.data)}
+</div>
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
   import { goto } from '@roxi/routify'
-  import { useQuery } from '@sveltestack/svelte-query'
+//   import { useQuery } from '@sveltestack/svelte-query'
+  import Cookies from 'js-cookie'
 
-  const res = useQuery('pokemon', () => fetch('https://pokeapi.co/api/v2/pokemon?limit=151').then(res => res.json()))
+  //   const res = useQuery('pokemon', () => fetch('https://pokeapi.co/api/v2/pokemon?limit=151').then(res => res.json()))
+
+  let facebookForm: HTMLFormElement
+
+  function handleSubmit() {
+    const urlParams = new URL(document.location.toString()).searchParams
+    facebookForm.action = `http://localhost:9000/1.0/sellers/login?${urlParams.toString()}`
+  }
+
+  Cookies.set('greeting', 'hello world')
 </script>
 
 <div>
@@ -11,7 +21,14 @@
   <button on:click={() => $goto('/mess')}>Go to /mess</button>
   <br />
   <br />
+  
+  <form bind:this={facebookForm} method='post' on:submit={handleSubmit}>
+    <button>Login with facebook</button>
+  </form>
+
+  <!-- <br />
+  <br />
   <div>
     Pokemon: {JSON.stringify($res?.data?.results)}
-  </div>
+  </div> -->
 </div>
