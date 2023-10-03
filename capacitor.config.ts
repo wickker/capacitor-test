49ca5c90c0a6f26a8a191d320ca11b59a50f6498
Dian Chun Choy <choydianchun@gmail.com>
2023-10-03 15:11:51 +0800

    auth and preferences work

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,12 +4,12 @@ const config: CapacitorConfig = {
   appId: 'capacitor.app',
   appName: 'capacitor',
   webDir: 'dist',
-  server: {
-    // input your local machine ip for live reload (this is logged out when you run yarn dev)
-    url: 'http://10.110.162.166:3000', 
-    // enable for local dev only
-    cleartext: true, 
-  },
+  // server: {
+  //   // input your local machine ip for live reload (this is logged out when you run yarn dev)
+  //   url: 'http://10.110.162.166:3000', 
+  //   // enable for local dev only
+  //   cleartext: true, 
+  // },
   plugins: {},
 }
 
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
   import { goto } from '@roxi/routify'
   import axios from 'axios'
-  import { useQuery } from '@sveltestack/svelte-query'
+  // import { useQuery } from '@sveltestack/svelte-query'
+  import { Preferences } from '@capacitor/preferences'
 
-  // Axios and svelte query POC:
-  // const api = axios.create({
-  //   baseURL: 'https://a32f-2401-7400-c807-9d0d-90be-9655-720-a33b.ngrok-free.app',
-  // })
-  // const getMe = useQuery('me', () =>
-  //   api.get(`1.0/sellers/test`).then((res) => res.data)
-  // )
+  let inputVal: string = ''
+  let data: string = ''
+  let token: string = ''
+
+  const tokenName = 'otterlive-token'
+
 </script>
 
-<div class="py-8 px-5">
+<div class="px-5 py-8">
   <p class="text-4xl">Root Page</p>
   <br />
 
@@ -28,5 +28,57 @@
   <br />
   <br />
 
-  <!-- {`Get data: ${JSON.stringify($getMe.data)}`} -->
+  <div>Preference:</div>
+  <input
+    class="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+    type="text"
+    bind:value={inputVal}
+  />
+  <button
+    class="mt-2 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+    on:click={async () => {
+      await Preferences.set({
+        key: tokenName,
+        value: inputVal,
+      })
+      inputVal = ''
+    }}
+  >
+    Set Preference
+  </button>
+  <br />
+  <br />
+
+  <button
+    class="mt-2 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+    on:click={async () => {
+      const { value } = await Preferences.get({key: tokenName})
+      token = value || ''
+    }}
+  >
+    Get Preference
+  </button>
+  <div>Preference '{tokenName}': {token}</div>
+  <br />
+  <br />
+
+  <button
+    class="mt-2 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+    on:click={async () => {
+      const res = await axios.get(
+        'https://b016-202-83-111-226.ngrok-free.app/1.0/sellers/mobile-auth',
+        {
+          headers: {
+            'Authorization': token,
+          },
+        }
+      )
+      data = JSON.stringify(res.data)
+    }}
+  >
+    Get Data
+  </button>
+  <br />
+  <div>Data: {data}</div>
+
 </div>
